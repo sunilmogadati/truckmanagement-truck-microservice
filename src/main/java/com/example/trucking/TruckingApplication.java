f5d34934265f936cb58dc12f11dd913ccfb136f9
Erik Jepsen <5465915+ejep520@users.noreply.github.com>
2022-10-07 16:46:03 -0700

    This is the baseline Spring framework based on team discussions

---
+++ src/main/java/com/example/trucking/TruckingApplication.java
@@ -0,0 +1,13 @@
+package com.example.trucking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TruckingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TruckingApplication.class, args);
+	}
+
+}
