b73a0023e7cf0f096292d028b21f7188dbf455f7
Pirai Ojeda (Rio) <87847753+riosol7@users.noreply.github.com>
2022-10-17 14:10:03 -0700

    eureka done

--- src/main/java/com/truck/TruckingApplication.java
+++ src/main/java/com/truck/TruckingApplication.java
@@ -2,10 +2,10 @@ package com.truck;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-//@EnableEurekaClient
+@EnableEurekaClient
 public class TruckingApplication {
 
   public static void main(String[] args) {
